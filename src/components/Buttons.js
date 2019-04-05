f223ebb38f7624fa59f9854bc888fc1f3c8a1ef9
github username <crishanks12@gmail.com>
2019-04-05 12:05:02 -0700

    Done.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,6 +5,7 @@ const Buttons = (props) => {
     <div className='ui eight wide column'>
       <button onClick={props.handleMoreInfoClick}>More Info</button>
       <button onClick={props.handleHideMeClick}>Hide Me</button>
+      <button onClick={props.handleHideHogClick}> Hide Hog</button>
     </div>
   )
 }
--- src/components/PigCard.js
+++ src/components/PigCard.js
@@ -5,35 +5,58 @@ import PigInfo from './PigInfo'
 class PigCard extends Component {
   constructor(props) {
     super(props)
-    this.state = {showing: false}
+    this.state = {
+      infoShowing: false,
+      hogShowing: true
+    }
   }
 
   handleMoreInfoClick = () => {
     console.log('handlemore')
-    this.setState({showing: true})
+    this.setState({infoShowing: true})
   }
 
   handleHideMeClick = () => {
     console.log('hidemore')
-    this.setState({showing: false})
+    this.setState({infoShowing: false})
   }
 
-  render() {
-    return (
-      <div className='ui card'>
+  handleHideHogClick = () => {
+    this.setState({hogShowing: false})
+  }
+
+  handleShowHogClick = () => {
+    this.setState({hogShowing: true})
+  }
+
+  showHog = () => {
+    if (this.state.hogShowing) {
+      return <div>
         <div className='image'>
-          <img src={this.props.hog.image} alt="pig image"/>
+            <img src={this.props.hog.image} alt="pig image"/>
         </div>
         <div className='content'>
           <h3>{this.props.hog.name}</h3>
         </div>
         <p>{this.props.hog.specialty}</p>
-        {this.state.showing ? <PigInfo hog={this.props.hog}/> : null }
+        <div>
+          {this.state.infoShowing ? <PigInfo hog={this.props.hog}/> : null }
+        </div>
         <Buttons 
           handleMoreInfoClick={this.handleMoreInfoClick}
           handleHideMeClick={this.handleHideMeClick}
+          handleHideHogClick={this.handleHideHogClick}
           hog={this.props.hog}
         />
+      </div>
+    }
+    return <button onClick={this.handleShowHogClick}> Show Hog</button>
+  }
+
+  render() {
+    return (
+      <div className='ui card'>
+        {this.showHog()}
       </div>
     )
   }
